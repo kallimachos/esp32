e3da67f3d9b4939c47249347a0494e141f2909f3
Brian Moss <kallimachos@gmail.com>
2020-05-28 21:49:01 +1000

    Add DHT sensor

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include "DHT.h"
 
 #define LEDPin 5
+#define DHTPIN 4
+#define DHTTYPE DHT11
 
 const char* ssid = "...";
-const char* password =  "...";
+const char* password = "...";
 
+DHT dht(DHTPIN, DHTTYPE);
 AsyncWebServer server(80);
 
 void setup(){
@@ -26,15 +30,37 @@ void setup(){
         request->send(200, "text/html", "<p>Some HTML!</p>");
     });
     server.begin();
+    Serial.println("DHT11 test!");
+    dht.begin();
 }
 
 void loop(){
-    digitalWrite(LEDPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LEDPin, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(200);
-    digitalWrite(LEDPin, LOW);    // turn the LED off by making the voltage LOW
+    digitalWrite(LEDPin, LOW);   // turn the LED off by making the voltage LOW
     delay(200);
     digitalWrite(LEDPin, HIGH);
     delay(200);
     digitalWrite(LEDPin, LOW);
-    delay(1000);
+    delay(1200);
+
+    float h = dht.readHumidity();
+
+    float t = dht.readTemperature();  // Read temperature as Celsius (the default)
+
+    if (isnan(h) || isnan(t)) {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+    }
+
+    // Compute heat index in Celsius (isFahreheit = false)
+    float hic = dht.computeHeatIndex(t, h, false);
+
+    Serial.print(F("Humidity: "));
+    Serial.print(h);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(t);
+    Serial.print(F("°C  Heat index: "));
+    Serial.print(hic);
+    Serial.println(F("°C"));
 }
